178afa912e13796c077606642699603b9ba83fe2
David Vu <vutrant@Davids-MacBook-Pro.local>
2023-11-21 13:14:36 -0500

    Map array to die components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { Die } from './components/die'
+
+function App() {
+  const [allNewDice, setAllNewDice] = useState(generateRandomNumbers())
+
+  function generateRandomNumbers() {
+    const randomNumbers = [];
+    for (let i = 0; i < 10; i++) {
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+        randomNumbers.push(randomNumber);
+    }
+    return randomNumbers;
+  }
+
+  return (
+    <main>
+      <div className='die-container'>
+        {allNewDice.map((e, i) => <Die key={i} value={e}/>)}
+      </div>
+    </main>
+  )
+}
+
+export default App
