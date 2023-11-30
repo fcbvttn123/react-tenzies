668feecfc78485ced429f65d3c0fbd84ac07cd9d
David Vu <vutrant@Davids-MacBook-Pro.local>
2023-11-30 18:13:27 -0500

    Initial Submission

---
+++ src/components/Dot.jsx
@@ -0,0 +1,5 @@
+export function Dot() {
+    return (
+        <div className="dot"></div>
+    )
+}
--- src/components/die.jsx
+++ src/components/die.jsx
@@ -1,10 +1,14 @@
+import { Dot } from "./Dot"
+
 export function Die(props) {
     let styles = {
         backgroundColor: props.isHeld ? "#59E391" : "#fff"
     }
     return (
         <div className="die-face" style={styles} onClick={() => props.holdDice(props.id)}>
-            <h2 className="die-num">{props.value}</h2>
+            {/* <h2 className="die-num">{props.value}</h2> */}
+            <Dot />
+            <Dot />
         </div>
     )
 }
